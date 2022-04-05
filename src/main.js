86f45e57e68604480e45df5bff1495c927a9e61c
Nathan Prieto <ncprieto@ucsc.edu>
2022-04-05 12:13:47 -0700

    Added scrolling background and missle

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ let config = {
     height: 480,
     scene: [Menu, Play]
 }
+
 let game = new Phaser.Game(config);
+
 let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
+
+// reserve keyboard vars
+let keyF, keyR, keyLEFT, keyRIGHT;
