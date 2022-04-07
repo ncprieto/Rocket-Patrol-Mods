5f1850e78653753ab8b763a576ea5165957024be
Nathan <ncprieto@ucsc.edu>
2022-04-07 12:57:07 -0700

    Added sound effects, menu reset, animations, etc.

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -2,9 +2,26 @@ class Menu extends Phaser.Scene{
     constructor(){
         super("menuScene");
     }
-
+    preload() {
+        // load audio
+        this.load.audio('sfx_select', './assets/blip_select12.wav');
+        this.load.audio('sfx_explosion', './assets/explosion38.wav');
+        this.load.audio('sfx_rocket', './assets/rocket_shot.wav');
+    }
     create(){
-        this.add.text(20, 20, "Rocket Patrol Menu");
-        this.scene.start("playScene");
+        let menuConfig = {
+            fontFamily: 'Courier',
+            fontSize: '28px',
+            backgroundColor: '#F3B141',
+            color: '#843605',
+            align: 'right',
+            padding: {
+            top: 5,
+            bottom: 5,
+            },
+            fixedWidth: 0
+        }
+
+        this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, 'Rocket Patrol', menuConfig).setOrigin(0.5);
     }
 }
