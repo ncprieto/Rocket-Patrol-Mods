9ccdf6ce79a1ef4777bbbd677597ba56f7a77b97
Nathan Prieto <ncprieto@ucsc.edu>
2022-04-08 10:16:47 -0700

    Final Version

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -54,6 +54,7 @@ class Play extends Phaser.Scene{
         // game over flag 
         this.gameOver = false;
         // 60-second play clock
+        // clock
         scoreConfig.fixedWidth = 0;
         this.clock = this.time.delayedCall(game.settings.gameTimer, () => {
             this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER', scoreConfig).setOrigin(0.5);
